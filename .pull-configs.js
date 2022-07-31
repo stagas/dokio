e655ac5580f5d0d634876aed8a253cc1d8611d78
stagas <gstagas@gmail.com>
2022-07-31 05:55:28 +0300

    chore: repo

--- .pull-configs.js
+++ .pull-configs.js
@@ -15,23 +15,52 @@ merge('package.json', (prev, next) => {
   prev.scripts = next.scripts
   prev.files = next.files
   sort(assign(prev.devDependencies, next.devDependencies))
+
+  // never used it - OTR screen acts like review so
+  // it can be accidentally published anyway. also i dont care
+  delete prev.private
+
+  // deprecated
+  delete prev.devDependencies['@stagas/documentation-fork']
+  delete prev.devDependencies['@rollup/plugin-commonjs']
+  delete prev.devDependencies['@stagas/sucrase-jest-plugin']
+  delete prev.devDependencies['@web/dev-server-esbuild']
+  delete prev.devDependencies['@web/dev-server-rollup']
+  delete prev.devDependencies['@web/test-runner']
+  delete prev.devDependencies['esbuild']
+  delete prev.devDependencies['esbuild-register']
+  delete prev.devDependencies['prettier']
+  delete prev.devDependencies['terser']
+  delete prev.devDependencies['vite-web-test-runner-plugin']
+  delete prev.devDependencies['@swc-node/jest']
+  delete prev.devDependencies['chokidar']
+  delete prev.devDependencies['jest']
+  delete prev.devDependencies['jest-browser-globals']
+  delete prev.devDependencies['ts-jest']
+  delete prev.devDependencies['ts-node']
+  delete prev.devDependencies['wtr-plugin-vite']
+  delete prev.devDependencies['@stagas/jest-node-exports-resolver']
 })
+replace('.gitattributes')
 replace('.gitignore')
+replace('.npmrc')
 replace('.eslintrc.js')
-// replace('.pull-configs.js')
+replace('.pull-configs.js')
 replace('.swcrc')
 replace('dprint.json')
-replace('jest.config.js')
 replace('tsconfig.json')
-// replace('tsconfig.dist.json')
-replace('web-test-runner.config.js')
+replace('tsconfig.dist.json')
+replace('LICENSE')
 
 const deprecated = [
   '.vscode/extensions.json',
   '.vscode',
   '.prettierrc',
   '.prettierignore',
+  'example/tsconfig.json',
   'vite.config.js',
+  'jest.config.js',
+  'web-test-runner.config.js',
 ]
 deprecated.forEach(x => {
   try {
